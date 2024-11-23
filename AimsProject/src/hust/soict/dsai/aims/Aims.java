4fd464e2cd1c8353085470d98f0b0b5003481b5f
EspiMKII <therealrobin14@gmail.com>
2024-11-23 15:21:08 +0700

    Added class & attribute member examples in DigitalVideoDisc.java, and some examples of that in Aims.java

--- AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -11,16 +11,23 @@ public class Aims {
 
         //Create a new DVD object and add them to the cart
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        anOrder.addDigitalVideoDisc(dvd1);
-        
+
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        anOrder.addDigitalVideoDisc(dvd2);
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        anOrder.addDigitalVideoDisc(dvd3);
+
+        DigitalVideoDisc[] dvdList = {dvd1, dvd2, dvd3};
+        anOrder.addDigitalVideoDisc(dvdList);
 
         //Print the cart and the total cost of the items in the cart
         anOrder.displayCart();
+        System.out.println("ID of \"The Lion King\" dvd is " + dvd1.getId());
+
+        // Lab03 section 5 information:
+        // anOrder.displayCart() only displays the titles of the DVDs in the cart
+        // This is an example of instance attributes in play: we only take the titles (instance attributes) of the DVDs in the cart
+        // dvd1's id is also an instance attribute, but it is determined by the class attribute nbDigitalVideoDiscs
+
         System.out.println("Total cost is: " + anOrder.totalCost() + "$");
     }
 }
