53b5f332c7d20a216281c174901c31a487aec2c1
EspiMKII <therealrobin14@gmail.com>
2024-11-24 15:57:36 +0700

    Finished section 7: Store class

--- AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -2,7 +2,6 @@ package hust.soict.dsai.aims;
 
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.disc.DigitalVideoDisc;
-import java.util.ArrayList;
 
 public class Aims {
     public static void main(String[] args) {
