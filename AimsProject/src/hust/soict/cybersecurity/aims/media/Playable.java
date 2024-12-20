a16394eca02c9652e136ce20ccc7df3ecbc061ae
EspiMKII <therealrobin14@gmail.com>
2024-12-20 14:46:46 +0700

    Empty commit

--- AimsProject/src/hust/soict/cybersecurity/aims/media/Playable.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.cybersecurity.aims.media;
-
-import hust.soict.cybersecurity.aims.exception.PlayerException;
-
-public interface Playable {
-	public void play() throws PlayerException;
-}
